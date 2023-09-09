a0b55fdd35e19b6b472546915aac96c55030c728
MatteoLoPiccolo <lostsoul81x@gmail.com>
2023-09-09 23:04:58 +0200

    Add Enemy script + Line Render component

--- Angry_Birds/Assets/Scripts/Bird.cs
+++ Angry_Birds/Assets/Scripts/Bird.cs
@@ -1,14 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] float maxDragDistance = 4f;
+    [SerializeField] float launchPower = 150f;
+
+    LineRenderer lineRenderer;
+    Vector3 startPosition;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, transform.position);
+        startPosition = transform.position;
+    }
+
     void OnMouseDrag() 
     {
         var destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         destination.z = 0;
+        if(Vector2.Distance(destination, startPosition) > maxDragDistance)
+            destination = Vector3.MoveTowards(startPosition, destination, maxDragDistance);
         transform.position = destination;
+        lineRenderer.SetPosition(1, transform.position);
+    }
+
+    void OnMouseUp()
+    {
+        Vector3 directionAndMagnitude = startPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionAndMagnitude * launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
     // Update is called once per frame
---
+++ Angry_Birds/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float destroyImpactMagnitude = 5f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Bird>() != null)
+            Destroy(gameObject);
+
+        if (collision.relativeVelocity.magnitude > destroyImpactMagnitude)
+            Destroy(gameObject);
+    }
+}
